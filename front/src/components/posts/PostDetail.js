6339f3eea17be7a1b93b7f8e5df69d381c3d5a2d
SimonFolkerts <simfolkerts@gmail.com>
2023-03-30 12:28:19 +1300

    add create form component and delete component to PostDetail view

--- front/src/components/posts/PostDetail.js
+++ front/src/components/posts/PostDetail.js
@@ -2,25 +2,35 @@ import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
 import PostDelete from "./PostDelete";
+import CommentCreate from "./CommentCreate";
+import CommentDelete from "./CommentDelete";
 
 const PostDetail = () => {
   const [post, setPost] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
-    const getPost = async () => {
-      const response = await fetch(`http://localhost:3001/posts/${id}`);
-      const data = await response.json();
-      setPost(data);
-    };
     getPost();
   }, []);
+
+  const getPost = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${id}`);
+    const data = await response.json();
+    setPost(data);
+  };
 
   const comments = post.comments?.map((comment) => {
     return (
       <div className="comment" key={comment._id}>
         <p>{comment.message}</p>
         <p>{comment.authorId}</p>
+        {localStorage.getItem("userId") === comment.authorId ? (
+          <CommentDelete
+            postId={post._id}
+            commentId={comment._id}
+            refreshComments={getPost}
+          />
+        ) : null}
       </div>
     );
   });
@@ -33,7 +43,6 @@ const PostDetail = () => {
         <div className="post__detail">
           <h3>{post.title}</h3>
           <p>{post.content}</p>
-          {comments}
 
           {/* if a user id that matches the authorId of the post is present in the localstorage,
           then that user must be logged in and therefore is allowed to edit and delete, so render the buttons */}
@@ -45,8 +54,12 @@ const PostDetail = () => {
               <PostDelete id={post._id} />
             </div>
           ) : null}
+          {localStorage.getItem("userId") ? (
+            <CommentCreate id={post._id} refreshComments={getPost} />
+          ) : null}
         </div>
       ) : null}
+      {comments}
     </div>
   );
 };
