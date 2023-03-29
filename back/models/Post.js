c182edc0407ccca0d8bd9a9cdd8c5151153f525d
SimonFolkerts <simfolkerts@gmail.com>
2023-03-30 09:21:09 +1300

    add comment model and embed it into post model

--- back/models/Post.js
+++ back/models/Post.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+import Comment from "./Comment.js";
+
+// add comment as a subdocument of post
 const postSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -7,6 +10,7 @@ const postSchema = new mongoose.Schema({
   },
   content: { type: String, required: true },
   authorId: { type: mongoose.ObjectId, required: true },
+  comments: [Comment],
 });
 
 module.exports = mongoose.model("Post", postSchema);
