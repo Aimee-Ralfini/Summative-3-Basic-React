8e9f2454682ae57ce21dafcb5132d09b4c700c5c
SimonFolkerts <simfolkerts@gmail.com>
2023-03-31 17:39:25 +1300

    remove go home link from contact page for consistency

--- front/src/pages/Contact.js
+++ front/src/pages/Contact.js
@@ -1,11 +1,9 @@
 // contact page
-import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
     <div className="page contact">
       <h1>Contact Page</h1>
-      <Link to="/">Go Home</Link>
     </div>
   );
 };
